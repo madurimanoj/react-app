a807f9979c0605994bcf839e19945becbe7d52a2
Manoj <manoj@Turtle-Jr-II.local>
2018-07-05 15:42:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 // import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
@@ -11,11 +12,10 @@ class App extends Component {
           <h1 className="App-title">Hello world</h1>
         </header>
         <p className="App-intro">
-
+        React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.
         </p>
       </div>
     );
   }
 }
-
 export default App;
